121b9a756d2353fa12b9ae73a03652325fd82576
Vadim <_s_e_t__@ukr.net>
2025-03-13 15:34:40 +0200

    add some code

--- models/product.js
+++ models/product.js
@@ -1,7 +1,7 @@
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, imageUrl, description) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
@@ -9,7 +9,31 @@ class Product {
   }
 
   save() {
-    
+    const db = getDb();
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
