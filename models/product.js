54a104f0b48b7542edab0d0ca0fd6eb8b8a96a44
Vadim <_s_e_t__@ukr.net>
2025-03-13 17:50:12 +0200

    add some code

--- models/product.js
+++ models/product.js
@@ -1,18 +1,26 @@
 const getDb = require("../util/database").getDb;
+const { ObjectId } = require("mongodb");
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: new ObjectId(this._id) },{$set:this});
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
       .then((result) => {
         console.log(result);
       })
@@ -28,8 +36,22 @@ class Product {
       .find()
       .toArray()
       .then((products) => {
-        console.log(products);
         return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(id) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new ObjectId(id) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
       })
       .catch((err) => {
         console.log(err);
